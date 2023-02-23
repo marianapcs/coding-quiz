07f41e74dfac97357b7eb9c6791a882cd66bc8b2
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-02-23 20:11:23 +0100

    adding questions, options and answers to the array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,34 @@
 //creating questions array
 
 let questions = [{
-    question: "",
+    question: "What is the element called that is used to describe the set of variables, objects and fucntions you have access to?",
     options: {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
+        1: "Restriction",
+        2: "Scope",
+        3: "Range",
+        4: "Output level",
     },
     answer: "",
+},
+{
+    question: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+    options: {
+        1: "Debugger",
+        2: "Clone",
+        3: "Loop",
+        4: "Repeater",
+    },
+    answer: "",
+},
+{
+    question: "Which method calls a function for each element in an array?",
+    options: {
+        1: "a. loop ()",
+        2: "b. while ()",
+        3: "c. each ()",
+        4: "d. forEach ()",
+    },
+    answer: "4",
 },
 {
     question: "",
@@ -21,33 +41,13 @@ let questions = [{
     answer: "",
 },
 {
-    question: "",
+    question: "Arrays in Javascript can be used to store ...?",
     options: {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
+        1: "a. strings",
+        2: "b. numbers",
+        3: "c. booleans",
+        4: "d. all of the above",
     },
-    answer: "",
-},
-{
-    question: "",
-    options: {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-    },
-    answer: "",
-},
-{
-    question: "",
-    options: {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-    },
-    answer: "",
+    answer: "4",
 },
 ]
