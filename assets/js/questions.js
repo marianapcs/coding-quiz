0028306642ea568cdc560063accfe7ee3f1e3c57
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-02-23 20:03:02 +0100

    created array with blank questions

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+//creating questions array
+
+let questions = [{
+    question: "",
+    options: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+    },
+    answer: "",
+},
+{
+    question: "",
+    options: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+    },
+    answer: "",
+},
+{
+    question: "",
+    options: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+    },
+    answer: "",
+},
+{
+    question: "",
+    options: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+    },
+    answer: "",
+},
+{
+    question: "",
+    options: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+    },
+    answer: "",
+},
+]
