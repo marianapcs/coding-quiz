58214c0a65194fd37cad7fe76b69dc2ae8042143
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-02-28 16:04:17 +0100

    created variables to get elements via their html IDs

---
+++ assets/js/logic.js
@@ -0,0 +1,7 @@
+//need to define the variables to get elements by ID
+var timer = document.getElementById("time");
+var startScreen = document.getElementById("start-screen");
+var startButton = document.getElementById("start");
+var questionEl = document.getElementById("questions");
+var questionEl = document.getElementById("questions");
+var questionEl = document.getElementById("questions");
