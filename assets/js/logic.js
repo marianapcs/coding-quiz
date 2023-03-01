a22283b27712b49b57a01f81bbb4f9a47c515fbf
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-03-01 16:44:34 +0100

    added timer to start quiz on click, doesn't come up with any errors, still figuring out how to start the quiz on t=click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,36 +1,58 @@
 //need to define the variables to get elements by ID
-var timer = document.getElementById("time");
-var startScreen = document.getElementById("start-screen");
+var startScreen = document.getElementById("#start-screen");
 var startButton = document.getElementById("start");
-var questionEl = document.getElementById("questions");
-var titleEl = document.getElementById("question-title");
-var optionEl = document.getElementById("choices");
-var end = document.getElementById("end-screen");
-var score = document.getElementById("final-score");
-var initials = document.getElementById("initials");
-var submit = document.getElementById("submit");
-var opinion = document.getElementById("feedback");
+var questionEl = document.getElementById("#questions");
+var optionEl = document.getElementById("#choices");
+var end = document.getElementById("#end-screen");
+var score = document.getElementById("#final-score");
+var initials = document.getElementById("#initials");
+var submit = document.getElementById("#submit");
+var opinion = document.getElementById("#feedback");
 
 // * A start button that when clicked a timer starts and the first question appears.
-// create a function that starts quix on clicking start and starts the timer
+startButton.addEventListener("click", timeLeft);
 
-let seconds = 60;
 
-function setTime () {
-    var timeLeft = setInterval (function (){
-        seconds --;
-        timer.textContent = seconds;
-//need to set it so that when timer reaches 0 the quiz finishes
-        if (secondsLeft <= 0) {
+// Timer that counts down from 60 seconds
+var timeLeft =60;
+var timerEl = document.getElementById("time");
+
+//setting the timer
+function countdown (){
+    var timeLeft = 60;
+    
+    //use setInterval() method to call the quiz to start
+    var timeInterval = setInterval(function(){
+        timeLeft --;
+        timer.textContent = timeLeft;
+        if (timeLeft <= 0) {
             clearInterval(timeLeft);
-            finishQuiz ();
+            end ();
         }
     }, 1000);
 }
- 
+
+// create a function that starts quiz on clicking start and starts the timer
+function quizStart(q) {
+    var titleEl = document.getElementById("#question-title");
+    titleEl.textContent = "";
+    titleEl.textContent = questions[q].question;
+    var button = document.querySelectorAll("button");
+    //declaring a block-scoped local variable, <less than 5, ++ increment with value set to start at 0
+    for (let x=0; x<5; x++)
+    
+}
 //   * Questions contain buttons for each answer.
 //Set variables to create buttons for each possible answer
-var 
+var answer1 = document.createElement("button");
+var answer2 = document.createElement("button");
+var answer3 = document.createElement("button");
+var answer4 = document.createElement("button");
+
+
+//need to set it so that when timer reaches 0 the quiz finishes
+       
+ 
 //   * 
 //   * When answer is clicked, the next question appears
 //   * 
