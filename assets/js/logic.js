4468f4a85fa80b6dc670082a28f38787f2287574
Mariana  Serrão <marianaserrao@Marianas-Air.home>
2023-03-07 12:06:11 +0100

    added functions for audio to play when answer is correct, added function for time to be subtracted when incorrect anseer chosen, questions array and function to loop through each question still won't work

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -26,7 +26,7 @@ function quizStart () {
             initialForm (); //create an initials form function when timer runs out to fill out your initials for the final score
         }
     }, 1000);
-    
+    quizQs ();
 }
 
 //need to set it so that when timer reaches 0 the quiz finishes
@@ -38,10 +38,11 @@ var timerEl = document.getElementById("time");
 
 //   * Questions contain buttons for each answer.
 //Set variables to create buttons for each possible answer
-var answer1 = document.createElement("button");
-var answer2 = document.createElement("button");
-var answer3 = document.createElement("button");
-var answer4 = document.createElement("button");
+// var answer2 = document.createElement("button");
+// var answer3 = document.createElement("button");
+// var answer4 = document.createElement("button");
+
+// var button = document.createElement("button");
 
 //create a function for your quiz questions to appear once you have clicked start
 var questionIndex = 0;
@@ -51,101 +52,121 @@ function quizQs (questionsArray) {
     var titleEl = document.getElementById("#question-title");
     titleEl.innerText = questions[questionIndex].question;
 //  titleEl.setAttribute("class", "display");
- 
-
- 
+    //need to turn options array into buttons within the function - more organised and hopefully more efficient
+    for (var i = 0; i < questions[questionIndex].options.length; i++ ){
+    const button = document.createElement("button");
+    button.innerText = questions[questionIndex].options[i];
+}
 }
 
-// function displayQuestions(questionsArray) {
-//   var questionIndex = 0;
-//   var questionContainer = document.getElementById("question-container");
-  
-//   function showQuestion() {
-//     questionContainer.innerHTML = questionsArray[questionIndex].question;
-//   }
-  
-//   showQuestion();
-  
-//   document.getElementById("next-button").addEventListener("click", function() {
-//     questionIndex++;
-//     if (questionIndex < questionsArray.length) {
-//       showQuestion();
-//     } else {
-//       questionContainer.innerHTML = "End of quiz";
-//     }
-//   });
-// }
 
-// var questions = [
-//   { question: "What is the capital of France?", answer: "Paris" },
-//   { question: "Who painted the Mona Lisa?", answer: "Leonardo da Vinci" },
-//   { question: "What is the highest mountain in the world?", answer: "Mount Everest" }
-// ];
-
-// displayQuestions(questions);
-
-
-
-
-//setting the timer
-// function countdown (){
-//     var timeLeft = 60;
-    
-//     //use setInterval() method to call the quiz to start
-//     var timeInterval = setInterval(function(){
-//         timeLeft --;
-//         timer.textContent = timeLeft;
-//         if (timeLeft <= 0) {
-//             clearInterval(timeLeft);
-//             end ();
-//         }
-//     }, 1000);
-// }
-
-// create a function that starts quiz on clicking start and starts the timer
-// function quizStart(q) {
-//     titleEl.textContent = "";
-//     titleEl.textContent = questions[q].question;
-//     var button = document.querySelectorAll("button");
-//     //declaring a block-scoped local variable, <less than 5, ++ increment with value set to start at 0
-//     for (let x=0; x<5; x++){
-//         button [x+1].textContent = questions[q].options[x+1];
-//     }
-//     opinion.textContent = "";
-//     return;
-    
-// }
-
-// var score = 0;
-
-
-
-       
- 
-//   * 
 //   * When answer is clicked, the next question appears
 //   * 
 //   * If the answer clicked was incorrect then subtract time from the clock
 
+function answer (choice, correctAnswer) {
+    if (choice === correctAnswer) {
+        score ++;
+    }
+    else {
+        timeLeft -= 10;
+    }
+
+
+}
+
 // * The quiz should end when all questions are answered or the timer reaches 0.
+
+function correctSound () {
+    var sound = new ("./assets/sfx/correct.wav");
+    sound.play ();
+}
+
+function incorrectSound () {
+    var sound = new ("./assets/sfx/correct.wav");
+    sound.play ();
+}
 
 //   * When the game ends, it should display their score and give the user the ability to save their initials and their score
 
 // signUpButton.addEventListener("click", function(event) {
-//     event.preventDefault();
-  
-//     var email = document.querySelector("#email").value;
-//     var password = document.querySelector("#password").value;
-  
-//     if (email === "") {
-//       displayMessage("error", "Email cannot be blank");
-//     } else if (password === "") {
-//       displayMessage("error", "Password cannot be blank");
-//     } else {
-//       displayMessage("success", "Registered successfully");
-  
-//       localStorage.setItem("email", email);
-//       localStorage.setItem("password", password);
-//       renderLastRegistered();
-//     }
+    //     event.preventDefault();
+    
+    //     var email = document.querySelector("#email").value;
+    //     var password = document.querySelector("#password").value;
+    
+    //     if (email === "") {
+        //       displayMessage("error", "Email cannot be blank");
+        //     } else if (password === "") {
+            //       displayMessage("error", "Password cannot be blank");
+            //     } else {
+                //       displayMessage("success", "Registered successfully");
+                
+                //       localStorage.setItem("email", email);
+                //       localStorage.setItem("password", password);
+                //       renderLastRegistered();
+                //     }
+
+
+      // function displayQuestions(questionsArray) {
+                //   var questionIndex = 0;
+                //   var questionContainer = document.getElementById("question-container");
+                  
+                //   function showQuestion() {
+                //     questionContainer.innerHTML = questionsArray[questionIndex].question;
+                //   }
+                  
+                //   showQuestion();
+                  
+                //   document.getElementById("next-button").addEventListener("click", function() {
+                //     questionIndex++;
+                //     if (questionIndex < questionsArray.length) {
+                //       showQuestion();
+                //     } else {
+                //       questionContainer.innerHTML = "End of quiz";
+                //     }
+                //   });
+                // }
+                
+                // var questions = [
+                //   { question: "What is the capital of France?", answer: "Paris" },
+                //   { question: "Who painted the Mona Lisa?", answer: "Leonardo da Vinci" },
+                //   { question: "What is the highest mountain in the world?", answer: "Mount Everest" }
+                // ];
+                
+                // displayQuestions(questions);
+                
+                
+                
+                
+                //setting the timer
+                // function countdown (){
+                //     var timeLeft = 60;
+                    
+                //     //use setInterval() method to call the quiz to start
+                //     var timeInterval = setInterval(function(){
+                //         timeLeft --;
+                //         timer.textContent = timeLeft;
+                //         if (timeLeft <= 0) {
+                //             clearInterval(timeLeft);
+                //             end ();
+                //         }
+                //     }, 1000);
+                // }
+                
+                // create a function that starts quiz on clicking start and starts the timer
+                // function quizStart(q) {
+                //     titleEl.textContent = "";
+                //     titleEl.textContent = questions[q].question;
+                //     var button = document.querySelectorAll("button");
+                //     //declaring a block-scoped local variable, <less than 5, ++ increment with value set to start at 0
+                //     for (let x=0; x<5; x++){
+                //         button [x+1].textContent = questions[q].options[x+1];
+                //     }
+                //     opinion.textContent = "";
+                //     return;
+                    
+                // }
+                
+                // var score = 0;
 //   });
