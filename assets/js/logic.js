de71ee94e7bea0451c2b0fc4536c0089f7f72396
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-03-13 12:47:05 +0100

    removed unecessary comments from logic.js, comments were other attempted methods for writing out the code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -132,30 +132,10 @@ submit.addEventListener("click",function (event){
 });
 
 
-// function scores () {
-//     initials.JSON.parse(initials.value);
-//     score = JSON.parse(timeLeft);
-// };
+
 
 
 
 //hide questions and go onto highscores page
 
 
-// signUpButton.addEventListener("click", function(event) {
-    //     event.preventDefault();
-    
-    //     var email = document.querySelector("#email").value;
-    //     var password = document.querySelector("#password").value;
-    
-    //     if (email === "") {
-        //       displayMessage("error", "Email cannot be blank");
-        //     } else if (password === "") {
-            //       displayMessage("error", "Password cannot be blank");
-            //     } else {
-                //       displayMessage("success", "Registered successfully");
-                
-                //       localStorage.setItem("email", email);
-                //       localStorage.setItem("password", password);
-                //       renderLastRegistered();
-                //     }
